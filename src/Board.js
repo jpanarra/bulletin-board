8b7ce6e0d644231783705ae3d92dde5124be4456
jpanarra <jpanarra@gmail.com>
2019-05-08 21:09:38 -0400

    Added the + button to create new notes in board.js

--- src/Board.js
+++ src/Board.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react'
 import Note from './Note'
+import { FaPlus } from 'react-icons/fa';
 
 class Board extends Component {
     constructor(props){
         super(props)
         this.state = {
-            notes: [
-                {
-                    id: 0, 
-                    note: "Call Lisa"
-                },
-                {
-                    id:1,
-                    note: "E-mail John"
-                },
-
-                {
-                    id:2,
-                    note: "Order Printer Ink"
-                }
-            ]
+            notes: []
         }
+        this.add = this.add.bind(this)
         this.eachNote = this.eachNote.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
+    }
+
+    add(text)  {
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: this.nextId(),
+                    note: text
+                }
+            ]
+        }))
+    }
+    nextId(){
+        this.uniqueId = this.uniqueID || 0
+        return this.uniqueID++
     }
 
     update(newText, i){
@@ -59,6 +64,8 @@ class Board extends Component {
         return(
                 <div className="board">
                     {this.state.notes.map(this.eachNote)}
+                    <button onClick = {this.add.bind(null, "New Note")}
+                            id= "add"><FaPlus /></button>
                 </div>
         )
     }
